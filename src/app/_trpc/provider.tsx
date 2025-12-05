f0fc058df66605537494da33130996b50ef2aa3d
RabbitBoii <chetanatram11284@gmail.com>
2025-12-05 12:40:10 +0530

    Fixed API url for deployment

--- src/app/_trpc/provider.tsx
+++ src/app/_trpc/provider.tsx
@@ -1,10 +1,21 @@
-"use client"; 
+"use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import { useState } from "react";
 import { trpc } from "./client";
 import superjson from "superjson";
+
+
+function getBaseUrl() {
+    if (typeof window !== "undefined") {
+        return "";
+    }
+    if (process.env.VERCEL_URL) {
+        return `https://${process.env.VERCEL_URL}`;
+    }
+    return "http://localhost:3000";
+}
 
 export default function TRPCProvider({ children }: { children: React.ReactNode }) {
     // Create a React Query Client 
@@ -21,8 +32,8 @@ export default function TRPCProvider({ children }: { children: React.ReactNode }
         trpc.createClient({
             links: [
                 httpBatchLink({
-                    url: "http://localhost:3000/api/trpc", 
-                    transformer: superjson, 
+                    url: `${getBaseUrl()}/api/trpc`,
+                    transformer: superjson,
                 }),
             ],
         })
